6bbd3fea276b09d6de86788fec3351cbc3a7c80f
nevada_scout <travis@impactmod.com>
2016-05-25 22:29:46 +0100

    Started on website homepage

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <link rel='stylesheet' type='text/css' href='/styles.css' />
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+    <title>trk.life - An Open-Source Time Tracker</title>
+
+    <!-- NOTE: Need https://github.com/vuejs/vue-resource for promises -->
+</head>
+
+<body class="landing">
+    <nav>
+        <ul>
+            <li class="logo"><a href="#">trk.life</a></li>
+
+            <li><a href="https://cloud.trk.life" class="btn btn-primary">Sign In</a></li>
+            <li><a href="https://github.com/trk-life" target="blank">GitHub</a></li>
+            <li><a href="/docs">Help / Docs</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/desktop">Desktop</a></li>
+            <li><a href="/web">Web</a></li>
+        </ul>
+    </nav>
+
+    <div class="hero">
+        <img class="screen" alt="Hero Screen" />
+        <h1>Finally. A well designed, open-source time tracker.</h1>
+        <h2>Don't conform - track time how suits you best.</h2>
+    </div>
+</body>
+</html>
